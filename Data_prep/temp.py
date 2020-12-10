e8f16168b6b270b0f9fde1aacd6efcdc9289bcf1
ckaraman <cagrkaraman@gmail.com>
2020-12-10 06:03:53 +0300

    Evaporation calculations
    minor revision

--- Data_prep/temp.py
+++ Data_prep/temp.py
@@ -27,14 +27,19 @@ ll = [str(i) for i in range(1, 13)]
 at = np.array(mmdlh).reshape(-1, len(mmdlh))
 
 
-def thorn(year):
+def thorn(year, df):
+    data = df.loc[df['yil'] == year]['hg45sck'].values
     lat = 37
     lat = deg2rad(lat)
     mmdlh = monthly_mean_daylight_hours(lat, year)
-    return pd.Series(mmdlh)
+    evapo = thornthwaite(data, mmdlh)
+    return evapo
     # df = pd.DataFrame(np.array(mmdlh).reshape(-1, len(mmdlh)), columns=[str(i) for i in range(1, 13)])
+
+
 at = pd.DataFrame()
-at['at'] = df.groupby('yil').apply(lambda x : thorn(x.name))
+at['at'] = df.groupby('yil').apply(lambda x: thorn(x.name))
+
 
 def doCalculation(df):
     groupCount = df.size
@@ -47,3 +52,12 @@ lat = deg2rad(57.1526)
 mmdlh = monthly_mean_daylight_hours(lat, year)
 monthly_t = [3.1, 3.5, 5.0, 6.7, 9.3, 12.1, 14.3, 14.1, 11.8, 8.9, 5.5, 3.8]
 thornthwaite(monthly_t, mmdlh)
+
+# tt = df[['ay', 'yil', 'hg45sck']]
+years = df['yil'].unique()
+years = years[:-1]
+df = df.loc[df['yil'].isin(years)]
+result = [thorn(i, df) for i in years]
+result = np.array(result).flatten()
+df['Evaporation'] = result
+
